4586bf85e6214d7dc963b22d3ffb6f7b33f7dadc
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-09-04 16:23:34 +0300

    asd

--- resources/views/layout/blog.blade.php
+++ resources/views/layout/blog.blade.php
@@ -5,88 +5,42 @@
                 <div class="blog-left">
                     <div class="section-title mb-55">
                         <div class="border-left">
-                            <p>Blog Posts</p>
+                            <p>@lang('front/blog.txt1')</p>
                         </div>
-                        <h2>Our Recent Blogs</h2>
+                        <h2>@lang('front/blog.txt2')</h2>
                     </div>
                 </div>
             </div>
             <div class="col-lg-6 col-md-4">
                 <div class="blog-right mb-30 f-right">
-                    <a href="blog.html.htm" class="grb-btn">Read More</a>
+                    <a href="{{ route('blog.index') }}" class="grb-btn">@lang('front/blog.txt3')</a>
                 </div>
             </div>
         </div>
         <div class="row wow fadeInUp">
-            <div class="col-lg-4 col-md-6">
-                <div class="blog-single mb-30 p-relative">
-                    <div class="blog-date">
-                        <div class="blog-date-shape">
-                            <img src="assets/img/shape/blog-date-shape.png" alt="">
+            @foreach ($blog as $blog)
+                <div class="col-lg-4 col-md-6">
+                    <div class="blog-single mb-30 p-relative">
+                        <div class="blog-date">
+                            <div class="blog-date-shape">
+                                <img src="{{ themeAsset('front', 'img/shape/blog-date-shape.png') }}" alt="">
+                            </div>
+                            <i class="fal fa-calendar-alt"></i>
+                            <span>{{ $blog->created_at->translatedFormat('d') }}</span>
+                            <p>{{ $blog->created_at->translatedFormat('M Y') }}</p>
                         </div>
-                        <i class="fal fa-calendar-alt"></i>
-                        <span>22</span>
-                        <p>Jan 2020</p>
-                    </div>
-                    <div class="blog-img">
-                        <a href="blog-details.html.htm"><img src="assets/img/blog/blog1.jpg"
-                                alt=""></a>
-                    </div>
-                    <div class="blog-content">
-                        <h4><a href="blog-details.html.htm">There Is a Risk On Investing Money For Finance
-                                Business</a></h4>
-                        <p>All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as
-                            [……]</p>
-                        <a href="blog-details.html.htm" class="read-btn">Read More</a>
+                        <div class="blog-img">
+                            <a href="{{ $blog->url }}"><img src="{{ $blog->getFirstMediaUrl('cover') }}"
+                                    alt=""></a>
+                        </div>
+                        <div class="blog-content">
+                            <h4><a href="{{ $blog->url }}">{{ $blog->title }}</a></h4>
+                            <p>{{ $blog->short_description }}</p>
+                            <a href="{{ $blog->url }}" class="read-btn">@lang('front/blog.txt4')</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="blog-single mb-30 p-relative">
-                    <div class="blog-date">
-                        <div class="blog-date-shape">
-                            <img src="assets/img/shape/blog-date-shape.png" alt="">
-                        </div>
-                        <i class="fal fa-calendar-alt"></i>
-                        <span>16</span>
-                        <p>Dec 2020</p>
-                    </div>
-                    <div class="blog-img">
-                        <a href="blog-details.html.htm"><img src="assets/img/blog/blog2.jpg"
-                                alt=""></a>
-                    </div>
-                    <div class="blog-content">
-                        <h4><a href="blog-details.html.htm">Seven Operational Tips & Tricks For Constriction
-                                Business</a></h4>
-                        <p>We adapt our delivery to the way your work, whether as an external provider or by
-                            [……]</p>
-                        <a href="blog-details.html.htm" class="read-btn">Read More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="blog-single mb-30 p-relative">
-                    <div class="blog-date">
-                        <div class="blog-date-shape">
-                            <img src="assets/img/shape/blog-date-shape.png" alt="">
-                        </div>
-                        <i class="fal fa-calendar-alt"></i>
-                        <span>07</span>
-                        <p>Nov 2020</p>
-                    </div>
-                    <div class="blog-img">
-                        <a href="blog-details.html.htm"><img src="assets/img/blog/blog3.jpg"
-                                alt=""></a>
-                    </div>
-                    <div class="blog-content">
-                        <h4><a href="blog-details.html.htm">Forging Relationships Between Multi-national
-                                Corporations</a></h4>
-                        <p>Over more than 20 years, we’ve fostered trusted relationships across government [……]
-                        </p>
-                        <a href="blog-details.html.htm" class="read-btn">Read More</a>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </section>
