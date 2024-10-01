51ad31935aef5a71a988ecfbd1f45ee159f86d31
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-10-01 22:28:11 +0300

    Update brand.blade.php

--- resources/views/layout/brand.blade.php
+++ resources/views/layout/brand.blade.php
@@ -1,75 +1,21 @@
-
-
 <div class="brand-area pt-100 pb-100">
     <div class="container">
         <div class="row wow fadeInUp">
             <div class="col-12">
                 <div class="swiper-container brand-active">
                     <div class="swiper-wrapper">
-                        <div class="swiper-slide">
-                            <div class="single-brand">
-                                <a href="#"><img src="assets/img/brand/brand1.png" alt=""></a>
-                                <a href="#"><img src="assets/img/brand/brand1-wc.png" alt=""></a>
+                        @foreach ($brands as $brand)
+                            <div class="swiper-slide">
+                                <div class="single-brand">
+                                    <a href="{{ $brand->url }}">
+                                        <img src="{{ $brand->image }}" alt="{{ $brand->title }}">
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="single-brand">
-                                <a href="#"><img src="assets/img/brand/brand2.png" alt=""></a>
-                                <a href="#"><img src="assets/img/brand/brand2-wc.png" alt=""></a>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="single-brand">
-                                <a href="#"><img src="assets/img/brand/brand3.png" alt=""></a>
-                                <a href="#"><img src="assets/img/brand/brand3-wc.png" alt=""></a>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="single-brand">
-                                <a href="#"><img src="assets/img/brand/brand4.png" alt=""></a>
-                                <a href="#"><img src="assets/img/brand/brand4-wc.png" alt=""></a>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="single-brand">
-                                <a href="#"><img src="assets/img/brand/brand5.png" alt=""></a>
-                                <a href="#"><img src="assets/img/brand/brand5-wc.png" alt=""></a>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="single-brand">
-                                <a href="#"><img src="assets/img/brand/brand1.png" alt=""></a>
-                                <a href="#"><img src="assets/img/brand/brand1-wc.png" alt=""></a>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="single-brand">
-                                <a href="#"><img src="assets/img/brand/brand2.png" alt=""></a>
-                                <a href="#"><img src="assets/img/brand/brand2-wc.png" alt=""></a>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="single-brand">
-                                <a href="#"><img src="assets/img/brand/brand3.png" alt=""></a>
-                                <a href="#"><img src="assets/img/brand/brand3-wc.png" alt=""></a>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="single-brand">
-                                <a href="#"><img src="assets/img/brand/brand4.png" alt=""></a>
-                                <a href="#"><img src="assets/img/brand/brand4-wc.png" alt=""></a>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="single-brand">
-                                <a href="#"><img src="assets/img/brand/brand5.png" alt=""></a>
-                                <a href="#"><img src="assets/img/brand/brand5-wc.png" alt=""></a>
-                            </div>
-                        </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
