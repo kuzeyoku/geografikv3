3e38ad7e8b8f09341a442a4a141b7c47aef9e2ee
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-10-01 23:13:42 +0300

    Update main.blade.php

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -7,7 +7,7 @@
     {!! SEO::generate() !!}
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="manifest" href="site.webmanifest">
-    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="@setting("asset","favicon")">
     <link rel="stylesheet" href="{{ themeAsset('front', 'css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ themeAsset('front', 'css/swiper-bundle.css') }}">
     <link rel="stylesheet" href="{{ themeAsset('front', 'css/animate.min.css') }}">
