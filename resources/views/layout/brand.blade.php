bb18de9b8c2e6f4164d82ff4ab6499bbb2d71002
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-10-01 22:47:47 +0300

    Update brand.blade.php

--- resources/views/layout/brand.blade.php
+++ resources/views/layout/brand.blade.php
@@ -1,4 +1,4 @@
-<div class="brand-area pt-100 pb-100">
+<div class="brand-area pt-50 pb-50">
     <div class="container">
         <div class="row wow fadeInUp">
             <div class="col-12">
@@ -7,6 +7,9 @@
                         @foreach ($brands as $brand)
                             <div class="swiper-slide">
                                 <div class="single-brand">
+                                    <a href="{{ $brand->url }}">
+                                        <img src="{{ $brand->image }}" alt="{{ $brand->title }}">
+                                    </a>
                                     <a href="{{ $brand->url }}">
                                         <img src="{{ $brand->image }}" alt="{{ $brand->title }}">
                                     </a>
@@ -19,3 +22,4 @@
         </div>
     </div>
 </div>
+
