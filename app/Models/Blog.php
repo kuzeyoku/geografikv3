f463fe4b1bfb4190a44b3ce5548c09325543c42b
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-10-02 09:09:04 +0300

    Update Blog.php

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -109,7 +109,7 @@ class Blog extends Model implements HasMedia
 
     public function getShortDescriptionAttribute(): string
     {
-        return Str::limit(strip_tags($this->description), 160);
+        return Str::limit(strip_tags($this->description), 250);
     }
 
     public function getMetaDescriptionAttribute(): string
