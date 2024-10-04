7efe3bfed6bfeab2f83b141e17cce25b1eeb7977
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-10-04 19:28:35 +0300

    a

--- app/Providers/CookieProvider.php
+++ app/Providers/CookieProvider.php
@@ -19,14 +19,6 @@ class CookieProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (setting("information", "cookie_notification_status") == \App\Enums\StatusEnum::Active->value) {
-            $page = cache()->remember("cookie_policy_page", config("cache.time"), function () {
-                return \App\Models\Page::findOrFail(setting("information", "cookie_policy_page"));
-            });
-            view()->composer("common.cookie_alert", function ($view) use ($page) {
-                $cookiePolicyPageLink = $page->url ?: null;
-                $view->with(compact("cookiePolicyPageLink"));
-            });
-        }
+        //
     }
 }
